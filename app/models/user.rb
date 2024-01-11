eb8dd490c5db3dc4832867a96b24eb098ba04599
Enoch Hsu <1529929+ewoknock@users.noreply.github.com>
2024-01-10 16:47:27 -0800

    Adds in Post model and creates relationship between User and Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+  validates :username, presence: true, length: { in: 4..20 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 8..12 }
 end
